54b375cae8149b9ba6914f6b5aabec6770712cfd
Ravitejapatnala <128287646+Ravitejapatnala@users.noreply.github.com>
2023-10-23 14:02:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function decimalToBinary(num) {
-  //Write you code here
-  
+  let binarynum= new Array(32);
+	let i=0;
+	while(num>0){ //7>0
+		binarynum[i]=num%2; 
+		num=Math.floor(num/2); 
+		i++;
+	}
+  for(let j= i-1; j>=0; j--)
+	  {
+		  console.log(binarynum[j]);
+	  }
 }
 
 window.decimalToBinary = decimalToBinary;
